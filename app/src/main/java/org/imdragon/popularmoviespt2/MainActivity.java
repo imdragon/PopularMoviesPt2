f81fe607abf087f252e66edf11da2ad16a5e43ed
imdragon <i.m.dragon@gmail.com>
2016-05-12 13:01:00 -0700

    Poster now showing up in first Fragment Mobile

--- app/src/main/java/org/imdragon/popularmoviespt2/MainActivity.java
+++ app/src/main/java/org/imdragon/popularmoviespt2/MainActivity.java
@@ -1,19 +1,14 @@
 package org.imdragon.popularmoviespt2;
 
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends FragmentActivity implements PosterFragment.OnFragmentInteractionListener{
-GridView gridView;
-    public ArrayList moviePosterAddress;
+public class MainActivity extends FragmentActivity implements PosterFragment.OnFragmentInteractionListener {
+    public ArrayList moviePosterAddress = new ArrayList();
     public ArrayList<Movie> movieObjectArray;
 
     @Override
@@ -21,22 +16,25 @@ GridView gridView;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.movie_posters);
 
+
         // check for to see which layout I'm using
-        if (findViewById(R.id.poster_container) != null){
-            gridView = (GridView) findViewById(R.id.gridview);
-            new RequestPopularMovies(this).execute(null,null,null);
+        if (findViewById(R.id.details_fragment) != null) {
             // if restoring don't do anything further so we don't layer something
-            if(savedInstanceState != null){
+            if (savedInstanceState != null) {
                 return;
             }
 
-            PosterFragment posterFragment = new PosterFragment();
-            // add these in case you need to start a new activity
-            posterFragment.setArguments(getIntent().getExtras());
+//            DetailsFragment detailsFragment = new DetailsFragment();
+//            // add these in case you need to start a new activity
+//            detailsFragment.setArguments(getIntent().getExtras());
+//
+//            // add the fragment to the layout
+//            getSupportFragmentManager().beginTransaction().add(R.id.details_fragment, detailsFragment).commit();
 
-            // add the fragment to the layout
-            getSupportFragmentManager().beginTransaction().add(R.id.poster_container, posterFragment).commit();
-        }
+
+        }PosterFragment posterFragment = new PosterFragment();
+        posterFragment.setArguments(getIntent().getExtras());
+        getSupportFragmentManager().beginTransaction().add(R.id.poster_container, posterFragment).commit();
 
     }
 
@@ -53,21 +51,6 @@ GridView gridView;
         outState.putParcelableArrayList("movies", movieObjectArray);
         super.onSaveInstanceState(outState);
     }
-
-    public void setupGrid() {
-        gridView.setAdapter(new ImageAdapter(MainActivity.this, moviePosterAddress));
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v,
-                                    int position, long id) {
-                Movie movieDetails = movieObjectArray.get(position);
-                Log.e("movieID", movieObjectArray.get(position).getMovieId());
-                Intent i = new Intent(MainActivity.this, DetailsFragment.class);
-                i.putExtra("movieInfo", movieDetails);
-                startActivity(i);
-            }
-        });
-    }
-
-
 }
+
+
--- app/src/main/java/org/imdragon/popularmoviespt2/PosterFragment.java
+++ app/src/main/java/org/imdragon/popularmoviespt2/PosterFragment.java
@@ -1,12 +1,26 @@
 package org.imdragon.popularmoviespt2;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 
 /**
@@ -22,6 +36,13 @@ public class PosterFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    GridView gridView;
+    int selectedMovie = 0;
+
+
+    protected ArrayList<String> moviePosterAddress = new ArrayList<>();
+    public ArrayList<Movie> movieObjectArray;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -64,7 +85,20 @@ public class PosterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_main, container, false);
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+        gridView = (GridView) view.findViewById(R.id.gridview);
+        if (savedInstanceState == null) {
+            new RequestPopularMovies().execute("popularity.desc", null, null);
+            Log.e("WAS NULL", "WAS NULL");
+        } else {
+            moviePosterAddress = savedInstanceState.getStringArrayList("posters");
+            movieObjectArray = savedInstanceState.getParcelableArrayList("movies");
+            selectedMovie = savedInstanceState.getInt("lastChoice", 0);
+            setupGrid();
+            Log.e("NOT NULL", "NOT NULL");
+        }
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -104,5 +138,96 @@ public class PosterFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    public void setupGrid() {
+
+        gridView.setAdapter(new ImageAdapter(getActivity(), moviePosterAddress));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+//                Movie movieDetails = movieObjectArray.get(position);
+//                Log.e("movieID", movieObjectArray.get(position).getMovieId());
+//                Intent i = new Intent(MainActivity.this, DetailsFragment.class);
+//                i.putExtra("movieInfo", movieDetails);
+//                startActivity(i);
+            }
+        });
+    }
+
+    public class RequestPopularMovies extends AsyncTask<String, Void, Void> {
+        StringBuilder total = new StringBuilder();
+        private JSONObject popArray;
+        private JSONArray movies;
+
+
+        final String MOVIE_BLOCKS = "results";
+        final String MOVIE_TITLE = "original_title";
+        final String MOVIE_POSTER = "poster_path";
+        final String MOVIE_BACKDROP = "backdrop_path";
+        final String MOVIE_OVERVIEW = "overview";
+        final String MOVIE_RATING = "vote_average";
+        final String MOVIE_RELEASE_DATE = "release_date";
+        final String MOVIE_ID = "id";
+        final String MOVIE_RUNTIME = "runtime";
+
+        public RequestPopularMovies() {
+
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+
+            // using the apikey in a separate string file to protect the apikey
+            try {
+                URL url = new URL("https://api.themoviedb.org/3/discover/movie?sort_by=" + params[0] + "&api_key=" + getString(R.string.apiKey));
+                // making url request and sending it to be read
+                Log.e("my url is", String.valueOf(url));
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                // preparing a reader to go through the response
+                BufferedReader r = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                // below allows for controlled reading of potentially large text
+                String line;
+                while ((line = r.readLine()) != null) {
+                    total.append(line);
+                }
+                //JSON section
+                popArray = new JSONObject(total.toString());
+                movies = popArray.getJSONArray(MOVIE_BLOCKS);
+                moviePosterAddress.clear();
+                movieObjectArray = new ArrayList<>();
+                // get movie poster filenames and put in an array
+                for (int i = 0; i < movies.length(); i++) {
+                    Movie tempMovie = new Movie();
+                    JSONObject aTitle = movies.getJSONObject(i);
+                    moviePosterAddress.add(aTitle.getString(MOVIE_POSTER));
+                    // check to see if working
+                    Log.e("poster path" + i, aTitle.getString(MOVIE_POSTER));
+                    // Build movie object
+                    tempMovie.setTitle(aTitle.getString(MOVIE_TITLE));
+                    tempMovie.setPoster(aTitle.getString(MOVIE_POSTER));
+                    tempMovie.setSynopsis(aTitle.getString(MOVIE_OVERVIEW));
+                    tempMovie.setRating(aTitle.getString(MOVIE_RATING));
+                    tempMovie.setReleaseDate(aTitle.getString(MOVIE_RELEASE_DATE));
+                    tempMovie.setBackdrop(aTitle.getString(MOVIE_BACKDROP));
+                    tempMovie.setMovieId(aTitle.getString(MOVIE_ID));
+                    movieObjectArray.add(tempMovie);
+
+                }
+                connection.disconnect();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            setupGrid();
+//            if(((PosterFragment) callingActivity).isDualPane){
+//                ((PosterFragment) callingActivity).updateDetailsFragment();
+//            }
+        }
     }
 }
