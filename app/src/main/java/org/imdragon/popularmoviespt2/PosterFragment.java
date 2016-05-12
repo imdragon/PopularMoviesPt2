541ee677090470bbb25fef075c3ba53a74b1b19f
imdragon <i.m.dragon@gmail.com>
2016-05-12 11:38:43 -0700

    Updated Poster Fragment

--- app/src/main/java/org/imdragon/popularmoviespt2/PosterFragment.java
+++ app/src/main/java/org/imdragon/popularmoviespt2/PosterFragment.java
@@ -64,7 +64,7 @@ public class PosterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_poster, container, false);
+        return inflater.inflate(R.layout.activity_main, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
